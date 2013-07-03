ce92db6065f2bc46b99ece7f3bc351ad1810e00a
Eduardo Felipe Castegnaro <edufelipe@e3c.com.br>
2013-07-03 18:57:48 -0300

    Add Question and Tag model

--- bufferoverflow/settings.py
+++ bufferoverflow/settings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'question',
 )
 
 # A sample logging configuration. The only tangible logging
--- question/models.py
+++ question/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    """Represents a question someone asked."""
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    """Adds a shoprt label for questions."""
+    question = models.ForeignKey(Question)
+    name = models.SlugField()
+
+    class Meta:
+        unique_together = ('question', 'name')
+
+    def __unicode__(self):
+        return self.name
