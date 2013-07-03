9320c3e3ec8f66e10a23831c32833fa3b235034b
Eduardo Felipe Castegnaro <edufelipe@e3c.com.br>
2013-07-03 20:26:45 -0300

    Adding question data to the home view

--- question/views.py
+++ question/views.py
@@ -1,5 +1,9 @@
 from django.template.response import TemplateResponse
+from .models import Question
 
 
 def home(request):
-    return TemplateResponse(request, 'base.html')
+    questions = Question.objects.all()
+    return TemplateResponse(request, 'question/list.html', {
+        'questions': questions,
+    })
