60122ffdb6c99a4092c6ac043ed39e688378d230
Eduardo Felipe Castegnaro <edufelipe@e3c.com.br>
2013-07-04 01:42:01 -0300

    Save tags on question creation

--- question/forms.py
+++ question/forms.py
@@ -1,8 +1,17 @@
-from django.forms import ModelForm
-from .models import Question
+from django.forms import ModelForm, CharField
+from .models import Question, Tag
 
 
 class QuestionForm(ModelForm):
+    tags = CharField(max_length=255)
+
     class Meta:
         model = Question
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'tags']
+
+    def save(self, *args, **kwargs):
+        q = super(QuestionForm, self).save(*args, **kwargs)
+        for name in self.cleaned_data['tags'].split(u','):
+            tag = Tag(question=q, name=name.strip())
+            tag.save()
+        return q
