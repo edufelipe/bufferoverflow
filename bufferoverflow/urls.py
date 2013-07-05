b591060147d40783444896771344b607e0210ae8
Eduardo Felipe Castegnaro <edufelipe@e3c.com.br>
2013-07-05 12:25:05 -0300

    Serve files belonging to users, such as the avatar

--- bufferoverflow/urls.py
+++ bufferoverflow/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,3 +13,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# Serve files belonging to users (such as their avatar)
+urlpatterns += static(settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
